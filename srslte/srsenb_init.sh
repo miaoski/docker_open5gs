d2a2e32127884f5832fa9ebc7c91f7cf71a2f099
miaoski <lin.zhemin@gmail.com>
2020-04-19 14:54:19 +0200

    move TX_GAIN RX_GAIN to .env

--- srslte/srsenb_init.sh
+++ srslte/srsenb_init.sh
@@ -6,3 +6,5 @@ cp /srsLTE/*.conf /etc/srslte/
 sed -i 's|MME_IP|'$MME_IP'|g' /etc/srslte/enb.conf
 sed -i 's|ENB_IP|'$ENB_IP'|g' /etc/srslte/enb.conf
 sed -i 's|DL_EARFCN|'$DL_EARFCN'|g' /etc/srslte/enb.conf
+sed -i 's|TX_GAIN|'$TX_GAIN'|g' /etc/srslte/enb.conf
+sed -i 's|RX_GAIN|'$RX_GAIN'|g' /etc/srslte/enb.conf
